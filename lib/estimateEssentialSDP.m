2f4adf9c270de6ddc5408097a1accc660dcb90a2
hankyang <hankyang@mit.edu>
2019-06-03 16:35:55 -0400

    update

--- lib/estimateEssentialSDP.m
+++ lib/estimateEssentialSDP.m
@@ -12,37 +12,52 @@ function [R_est,t_est,E_est,f_sdp,f_round,Z_sdp,solverOutput] = estimateEssentia
 dim_e = 9;
 dim_t = 3;
 dim_x = dim_e + dim_t;
-C = zeros(dim_x,dim_x);
+C = zeros(dim_e,dim_e);
+% first calculate C based on Eq. (12)
+% TODO
 
+% Build Q from C by adding zeros
+Q = zeros(dim_x,dim_x);
+Q = [C, zeros(dim_e,dim_t); zeros(dim_t,dim_e), zeros(dim_t,dim_t)];
 
-
-%% Generate constraint matrices Q (should be 7 constant matrices)
+%% Generate constraint matrices A (should be 7 constant matrices)
 % x = [e\tran, t\tran]\tran, dimension 12, each Q should also be dimension
 % 12 by 12
 numCons = 7;
-Q = zeros(dim_x,dim_x,numCons);
+A = zeros(dim_x,dim_x,numCons);
 % Manually build each of the 7 sparse constraint matrices
+% build A1 (15a)
+A1 = zeros(dim_x,dim_x);
+A1(1,1)=1; A1(4,4)=1; A1(7,7)=1; A1(11,11)=-1; A1(12,12)=-1;
+A(:,:,1) = A1;
+% build A2 (15b)
+
 
 
 
 %% Solve using CVX interface
-cvx_solver mosek
+cvx_solver mosek % sedumi, sdpt3
 cvx_begin sdp
 cvx_precision best
 variable Z(dim_x,dim_x) symmetric
 % the cost function
-minimize( trace(C * Z) )
+minimize( trace(Q * Z) )
 % the constraints
 subject to
 Z == semidefinite(dim_x);
 for i=1:numCons
-    trace(Q(:,:,i) * Z) == 0;
+    trace(A(:,:,i) * Z) == 0;
 end
 cvx_end
 
 %% Obtain solution from SDP result, calcualte rank 
 Z_sdp = Z;
 rank_Z_sdp = rank(Z_sdp,1e-3);
+if rank_Z_sdp == 1
+    fprintf('Rank = 1, SDP solves the original QCQP.\n');
+else
+    warning('Rank = %g, possibly wrong solution.\n',rank_Z_sdp);
+end
 % if rank=1, do eigen decomposition and recover x, e, and t
 
 
